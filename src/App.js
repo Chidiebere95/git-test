75e4d1e80606b611bfb357cebae043b90403165f
Chidiebere95 <chidiezeokwelume@gmail.com>
2023-06-05 17:04:48 +0100

    switch to master branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div className='App'>
+      <div>Hello world</div>
+      <p>Switch to master branch</p>
+    </div>
+  );
 }
 
 export default App;
