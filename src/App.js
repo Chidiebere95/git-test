f642a524e78d6dbff4aaf00fe5943df98d5ea1e0
Chidiebere95 <chidiezeokwelume@gmail.com>
2023-06-05 16:53:07 +0100

    wrote branch-2/created a new branch: branch-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello world</div>;
+  return (
+    <div className='App'>
+      <div>Hello world</div>
+      <p>added branch-2</p>
+    </div>
+  );
 }
 
 export default App;
